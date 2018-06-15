588aeef51421ffc5b4f50f7822ed75dbbbe41b09
Jan Tomsa <jan.tomsa.1976@gmail.com>
2018-06-15 13:44:53 +0200

    Better HTML parsing (thanks Monika Vlckova)

--- check_szr_web/check_szr_web.py
+++ check_szr_web/check_szr_web.py
@@ -1,5 +1,8 @@
 import urllib.request
 from bs4 import BeautifulSoup
+from datetime import datetime
+
+URL = "http://www.szrcr.cz/"
 
 try:
     orig_file = open("szr_marquee.txt", "r")
@@ -8,10 +11,15 @@ try:
 except FileNotFoundError:
     orig_text = ""
 
-soup = BeautifulSoup(urllib.request.urlopen('http://www.szrcr.cz/'), "lxml")
-new_tag = soup.find('marquee')
+soup = BeautifulSoup(urllib.request.urlopen(URL), "html.parser")
+div_scroll = soup.find_all("div", id="scroll")
+#new_tag = soup.find('marquee')
 #new_tag = soup.find(id="hp-text-scroll")
-new_text = str(new_tag)
+
+for tag in div_scroll:
+    new_text = tag.find('p').get_text()
+
+#new_text = str(new_tag)
 
 if orig_text == new_text:
     print('Marquee unchanged.')
@@ -21,12 +29,21 @@ else:
     print(orig_text)
     print('New text:')
     print(new_text)
-    file = open("szr_marquee.txt","w")
-    file.write(new_text)
-    file.close()
-    script = open("display_warning.vbs","w")
-    script.write('Set WshShell = WScript.CreateObject("WScript.Shell")\n')
-    script.write('WshShell.Run("http://www.szrcr.cz/")\n')
-    script.write('WScript.Sleep 1500\n')
-    script.write('WScript.Echo "SZR warning changed."\n')
-    script.close()
+
+    with open("szr_marquee.csv","a") as f:
+        sNow= datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        f.write("{0};{1}\n".format(sNow, new_text))
+        f.close()
+        print('Marquee changed!')
+        print(new_text)
+
+    with open("szr_marquee.txt","w") as file:
+        file.write(new_text)
+        file.close()
+
+    with open("display_warning.vbs","w") as script:
+        script.write('Set WshShell = WScript.CreateObject("WScript.Shell")\n')
+        script.write('WshShell.Run("http://www.szrcr.cz/")\n')
+        script.write('WScript.Sleep 1500\n')
+        script.write('WScript.Echo "SZR warning changed."\n')
+        script.close()
